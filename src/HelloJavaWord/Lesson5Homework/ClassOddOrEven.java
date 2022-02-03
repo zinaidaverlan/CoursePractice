3f323a70e653631c7d6dbb5390c592163e543625
verlanzinaida <verlan.zinaida@gmail.com>
2022-02-03 15:32:54 +0200

    Commit lesson5 ClassOddEven

---
+++ src/HelloJavaWord/Lesson5Homework/ClassOddOrEven.java
@@ -0,0 +1,18 @@
+package HelloJavaWord.Lesson5Homework;
+
+import java.util.Scanner;
+
+public class ClassOddOrEven {
+    public static void main(String[] args) {
+        System.out.println("Input an integer number:");
+
+        Scanner ObjNumber = new Scanner(System.in);
+
+        Integer number = ObjNumber.nextInt();
+        if (number % 2 == 0)
+            System.out.println("number: " + number + " is Odd");
+        else System.out.println("number: " + number + " is Even");
+
+    }
+
+}
