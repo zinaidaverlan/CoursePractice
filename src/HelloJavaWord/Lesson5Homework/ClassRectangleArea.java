a9b3c95882d99e61567c51182d1b9310676e3f26
verlanzinaida <verlan.zinaida@gmail.com>
2022-02-03 20:41:48 +0200

    Commit lesson5 Rectangle Area

--- src/HelloJavaWord/Lesson5Homework/ClassRectangleArea.java
+++ src/HelloJavaWord/Lesson5Homework/ClassRectangleArea.java
@@ -8,25 +8,36 @@ public class ClassRectangleArea {
         Scanner lengthObj = new Scanner(System.in);
 
         System.out.println("Input Width  for Rectangle 1:");
-        double width1 = widthObj.nextDouble();
+        int width1 = widthObj.nextInt();
         System.out.println("Input Length  for Rectangle 1:");
-        double length1 = lengthObj.nextDouble();
+        int length1 = lengthObj.nextInt();
 
         System.out.println("Input Width  for Rectangle 2:");
-        double width2 = widthObj.nextDouble();
+        int width2 = widthObj.nextInt();
         System.out.println("Input Length  for Rectangle 2:");
-        double length2 = lengthObj.nextDouble();
+        int length2 = lengthObj.nextInt();
+        boolean test=true;
 
-        double area1 = width1 * length1;
-        double area2 = width2 * length2;
+        while (test){
+        if (((width1 <= 0) || (length1 <= 0)) || ((width2 <= 0) || (length2 <= 0)))
+            {
+                System.out.println("This isn't an rectangle");
+                break;
+            }
+        else{
 
-        if (area1 > area2)
-            System.out.println("The Area of the Rectangle1 is greater than Area of the Rectangle 2");
-        else if (area1 < area2)
-            System.out.println("The Area of the Rectangle2 is greater than Area of the Rectangle 1");
-        else
-            System.out.println("The Area of the Rectangles are equal");
+            int area1 = width1 * length1;
+            int area2 = width2 * length2;
+
+            if (area1 > area2)
+                System.out.println("The Area of the Rectangle1 is greater than Area of the Rectangle 2");
+            else if (area1 < area2)
+                System.out.println("The Area of the Rectangle2 is greater than Area of the Rectangle 1");
+            else
+                System.out.println("The Area of the Rectangles are equal");}
+        test = false;
 
 
+        }
     }
 }
